055e7760d354ec84bfbc48de512179001fbe586a
Ridhsuki <basukiridhoalghifary@gmail.com>
2026-06-25 19:47:58 +0700

    feat(db): Menambahkan Attributes Pada Table

--- database/migrations/2026_06_24_065826_create_courses_table.php
+++ database/migrations/2026_06_24_065826_create_courses_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,14 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('path_trailer');
+            $table->text('about');
+            $table->string('thumbnail');
+            $table->foreignId('teacher_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
