111ad2619f90a827c450763934ef4e614d5f3977
alvn168 <justtada9@gmail.com>
2026-06-27 15:41:50 +0700

    chore(model): Persiapan Mass Assignment

--- app/Models/CourseStudent.php
+++ app/Models/CourseStudent.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CourseStudent extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'course_id',
+    ];
+
 }
